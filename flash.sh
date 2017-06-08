bf49c9a75e04316b7c7efcd04f5d1401772f1e59
Endolf <endolf@computerbooth.com>
2017-06-08 18:26:11 +0100

    Set the GPU memory

--- flash.sh
+++ flash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -eox pipefail
 
 usage()
 {
@@ -11,6 +11,7 @@ Flash a Kubernetes node SD card image.
 
 OPTIONS:
   -h    Show this message
+  -d    Device to flash the image to.
   -n    Set hostname for this SD image (master will generate a master image, otherwise nodes)
 EOF
   exit 1
@@ -20,9 +21,10 @@ if [ $# -lt 1 ]; then
   usage
 fi
 
-while getopts ":n:" opt; do
+while getopts ":n:d:" opt; do
   case $opt in
-    n)  IMAGE_HOSTNAME=$OPTARG ;;
+    n) IMAGE_HOSTNAME=$OPTARG ;;
+    d) device=$OPTARG ;;
   esac
 done
 
@@ -32,6 +34,9 @@ if [[ -z "${IMAGE_HOSTNAME// }" ]]; then
 fi
 
 pushd build > /dev/null
+
+mkdir -p boot
+mkdir -p root
 
 if [ ! -f raspbian_lite_latest.zip ]; then
   curl -L https://downloads.raspberrypi.org/raspbian_lite_latest -o raspbian_lite_latest.zip
@@ -45,8 +50,11 @@ image=`ls *-raspbian-*-lite.img`
 echo "Using $image"
 df -h
 
-read -p "Which device should the image be written to: "
-device="${REPLY}"
+if [[ -z "${device// }" ]]; then
+    read -p "Which device should the image be written to: "
+    device="${REPLY}"
+fi
+
 [[ ${device} != /dev/* ]] && device="/dev/${device}"
 [[ ${device} == /dev/mmcblk*p* ]] && device=`echo ${device} | sed -r 's/\/dev\/mmcblk([[:digit:]]+)p([[:digit:]]+)/\/dev\/mmcblk\1/'`
 [[ ${device} == /dev/sd* ]] && device=`echo ${device} | sed -r 's/\/dev\/sd([[:alpha:]]+)([[:digit:]]+)/\/dev\/sd\1/'`
@@ -68,13 +76,44 @@ fi
 
 for partition in $(df | grep "${device}" | cut -d " " -f1)
 do
-  sudo umount $partition
+  echo "Unmounting $partition"
+#  sudo umount $partition
 done
 
 image_size=`ls -lh $image | cut -d " " -f 5`
 
 echo "Flashing $image_size..."
-sudo dd status=progress bs=4M if=$image of=$device
+#sudo dd status=progress bs=4M if=$image of=$device
+
+udevadm settle
+
+#sudo hdparm -z $device
+
+if [[ ${device} == /dev/mmcblk* ]]; then
+  boot_partition="${device}p1"
+  root_partition="${device}p2"
+else
+  boot_partition="${device}1"
+  root_partition="${device}2"
+fi
+
+FS_TYPE=$(sudo blkid -o value -s TYPE "${boot_partition}" || echo vfat)
+echo "Mounting $boot_partition"
+#sudo mount -t ${FS_TYPE} "${boot_partition}" boot
+FS_TYPE=$(sudo blkid -o value -s TYPE "${root_partition}" || echo vfat)
+echo "Mounting $root_partition"
+#sudo mount -t ${FS_TYPE} "${root_partition}" root
+
+grep -q gpu_mem boot/config.txt &&
+    sudo sed -ri 's/^gpu_mem=.*$/gpu_mem=16/' boot/config.txt || echo -e "\n# Set GPU memory\ngpu_mem=16" | sudo tee --append boot/config.txt
+
+for partition in $(df | grep "${device}" | cut -d " " -f1)
+do
+  echo "Unmounting $partition"
+  #sudo umount $partition
+done
 
 popd > /dev/null
+echo "Done"
+
 exit 0
